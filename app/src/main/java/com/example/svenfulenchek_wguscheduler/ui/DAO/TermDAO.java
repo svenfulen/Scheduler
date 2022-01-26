2d89b6f66baf4143f20ad62326c3b74c460d7266
svenfulen <sfulen99@gmail.com>
2022-01-26 11:32:01 -0600

    Created updateTermDetailsById query

--- app/src/main/java/com/example/svenfulenchek_wguscheduler/ui/DAO/TermDAO.java
+++ app/src/main/java/com/example/svenfulenchek_wguscheduler/ui/DAO/TermDAO.java
@@ -19,13 +19,13 @@ public interface TermDAO {
     @Update
     void update(Term term);
 
-    @Query("")
-    void updateTermDetailsById(int termID);
-
     @Delete
     void delete(Term term);
 
     @Query("SELECT * FROM TERMS")
     public List<Term> getAllTerms();
 
+    // TODO: Test this function.  I don't know if it works but this is basically what it should do.
+    @Query("UPDATE TERMS " + "SET termTitle = :termTitle, startDate = :termStart, endDate = :termEnd " + "WHERE termId = :termID" )
+    void updateTermDetailsById(int termID, String termTitle, String termStart, String termEnd);
 }
